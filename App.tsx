7857717974bab3f8a915dc230995b8acf702d1a9
Andi Emma Davies <andidavies92@outlook.com>
2019-05-15 00:31:32 +0100

    Adjust App.tsx to conform to house style

--- App.tsx
+++ App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello, kinky TypeScript world!</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>Hello, kinky and tidy TypeScript world!</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white'
-  }
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "black"
+    },
+    text: {
+        color: "white"
+    }
 });
